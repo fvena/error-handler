89f61f0e3c3b963618a3f16a4adb3cb568b5ade2
Francisco Vena <fvena32@gmail.com>
2025-01-16 09:45:20 +0100

    feat: improve error logging format and enhance log detail output

--- src/utils/logs.ts
+++ src/utils/logs.ts
@@ -1,63 +1,97 @@
-import type { ErrorType, HandlerErrorProperties, Severity } from "../types";
+import type { ErrorType, HandlerErrorProperties } from "../types";
 import chalk from "chalk";
 
 /**
  * Formats the type of an error into a string with color.
  *
  * @param type - The type of the error.
- * @param severity - The severity of the error.
+ * @param data - The data to format.
  * @returns The formatted error type string.
  */
-export function formatErrorType(type: ErrorType, severity?: Severity): string {
-  const severityString = severity ? `${severity.toUpperCase()} ` : "";
+export function formatErrorType(type: ErrorType, data: string): string {
   const typeMap = {
-    error: chalk.red(`${severityString}Error:`),
-    warning: chalk.yellow(`${severityString}Warning:`),
+    error: chalk.red(data),
+    warning: chalk.yellow(data),
   };
 
   return Object.prototype.hasOwnProperty.call(typeMap, type)
     ? typeMap[type]
-    : chalk.red(`${severityString}Unknown Error:`);
+    : chalk.red("Unknown Error");
 }
 
 /**
  * Logs a compact representation of an error, suitable for centralized logging systems.
  *
  * @param data - The input data containing details of the error.
+ *
+ * @example
+ * timestamp [method] SEVERITY ERROR_TYPE errorCode file: message
  */
 export function logCompact(data: HandlerErrorProperties) {
   const logError = [];
 
   logError.push(chalk.gray(data.timestamp.toISOString()));
   if (data.method) logError.push(chalk.yellow(`[${data.method}]`));
-  logError.push(formatErrorType(data.type, data.severity));
+  const severity = data.severity ? data.severity.toUpperCase() : "";
+  const logErrorInfo =
+    [severity, toUpperCaseFirstLetter(data.type)].filter(Boolean).join(" ") + ":";
+  logError.push(formatErrorType(data.type, logErrorInfo));
   if (data.errorCode) logError.push(chalk.green(data.errorCode));
   if (data.file) logError.push(chalk.gray(data.file));
   logError.push(data.message);
 
-  console.log(logError.join(" "));
+  console.log(logError.filter(Boolean).join(" "));
 }
 
 /**
  * Logs detailed information about an error.
  *
  * @param data - The input data containing details of the error.
+ *
+ * @example
+ *
+ * errorName [SEVERITY ERROR_TYPE errorCode] method file: message
+ *
+ * Description:
+ * context message
+ * Received value: values
+ *
+ * Solution:
+ * solution
+ *
+ * Example:
+ * example
+ *
+ * Stack Trace:
+ * ❯ method file:line
+ * ❯ method file:line
  */
 export function logDetail(data: HandlerErrorProperties) {
-  const severity = formatErrorType(data.type, data.severity);
-  const errorCode = data.errorCode ? chalk.yellow(` ${data.errorCode}`) : "";
+  // Log header
+  const severity = data.severity ? data.severity.toUpperCase() : "";
+  const type = `${toUpperCaseFirstLetter(data.type)}${data.errorCode ? ":" : ""}`;
+  const errorCode = data.errorCode ? chalk.yellow(data.errorCode) : "";
   const method = data.method ? chalk.green(data.method) : "";
-  const file = data.file ? chalk.gray(` ${data.file}`) : "";
+  const file = data.file ? chalk.gray(data.file) : "";
   const message = chalk.bold(data.message);
 
-  // Log header
-  console.log(`${data.name} [${severity}${errorCode}] ${method}${file}: ${message}`);
+  const logHeaderErrorInfo = formatErrorType(
+    data.type,
+    `[${[severity, type, errorCode].filter(Boolean).join(" ")}]`,
+  );
+  const logHeaderLocation = `${[method, file].filter(Boolean).join(" ")}${method || file ? ":" : ""}`;
+  const logHeader = [data.name, logHeaderErrorInfo, logHeaderLocation, message]
+    .filter(Boolean)
+    .join(" ");
 
-  // Log values
+  console.log(logHeader);
+
+  // Log description
   console.log(chalk.bold(`\nDescription:`));
   console.log(chalk.gray(`${data.context ?? ""} ${data.message}`));
   if (data.values) {
-    console.log(chalk.gray(JSON.stringify(data.values)));
+    // eslint-disable-next-line unicorn/no-null -- null is a valid value
+    console.log(`Received value: ${chalk.gray(JSON.stringify(data.values, null, 2))}`);
   }
 
   // Log solution
@@ -80,7 +114,9 @@ export function logDetail(data: HandlerErrorProperties) {
     .map((trace) => {
       const prefix = chalk.gray(" ❯");
       const method = trace.method ? chalk.yellow(trace.method) : "<anonymous>";
-      const location = chalk.gray(`${trace.file ?? "undefine"}:${String(trace.line)}`);
+      const location = chalk.gray(
+        `${trace.file ?? "undefined"}:${String(trace.line ?? "unknown")}`,
+      );
       return `${prefix} ${method} ${location}`;
     })
     .join("\n");
@@ -92,6 +128,10 @@ export function logDetail(data: HandlerErrorProperties) {
  * Logs a simple representation of an error, suitable for quick overviews.
  *
  * @param data - The input data containing details of the error.
+ *
+ * @example
+ * [library] ErrorType: context message solution
+ * Received value: values
  */
 export function logSimple(data: HandlerErrorProperties): void {
   const logError = [];
@@ -100,7 +140,7 @@ export function logSimple(data: HandlerErrorProperties): void {
     logError.push(chalk.yellow(`[${data.library}]`));
   }
 
-  logError.push(formatErrorType(data.type));
+  logError.push(formatErrorType(data.type, `${toUpperCaseFirstLetter(data.type)}:`));
 
   if (data.context) {
     logError.push(data.context);
@@ -113,8 +153,13 @@ export function logSimple(data: HandlerErrorProperties): void {
   }
 
   if (data.values) {
-    logError.push(`\nReceived value: ${chalk.gray(JSON.stringify(data.values))}`);
+    // eslint-disable-next-line unicorn/no-null -- null is a valid value
+    logError.push(`\nReceived value: ${chalk.gray(JSON.stringify(data.values, null, 2))}`);
   }
 
-  console.log(logError.join(" "));
+  console.log(logError.filter(Boolean).join(" "));
+}
+
+function toUpperCaseFirstLetter(string_: string): string {
+  return string_.charAt(0).toUpperCase() + string_.slice(1);
 }
--- test/logs.test.ts
+++ test/logs.test.ts
@@ -23,28 +23,24 @@ const mockErrorData: HandlerErrorProperties = {
   values: undefined,
 };
 
+/**
+ * Format error type
+ */
 describe("formatErrorType", () => {
   it("should format error type without severity", () => {
-    const result = formatErrorType("error");
+    const result = formatErrorType("error", "Error:");
     expect(result).toBe(chalk.red("Error:"));
   });
 
   it("should format warning type without severity", () => {
-    const result = formatErrorType("warning");
+    const result = formatErrorType("warning", "Warning:");
     expect(result).toBe(chalk.yellow("Warning:"));
-  });
-
-  it("should format error type with severity", () => {
-    const result = formatErrorType("error", "high");
-    expect(result).toBe(chalk.red("HIGH Error:"));
-  });
-
-  it("should format warning type with severity", () => {
-    const result = formatErrorType("warning", "low");
-    expect(result).toBe(chalk.yellow("LOW Warning:"));
   });
 });
 
+/**
+ * Log simple error
+ */
 describe("logSimple", () => {
   it("should log simple error with all properties", () => {
     const consoleSpy = vi.spyOn(console, "log").mockImplementation(() => {
@@ -63,7 +59,7 @@ describe("logSimple", () => {
     logSimple(data);
 
     expect(consoleSpy).toHaveBeenCalledWith(
-      `${chalk.yellow("[TestLib]")} ${chalk.red("Error:")} TestContext TestMessage TestSolution \nReceived value: ${chalk.gray('{"key":"value"}')}`,
+      `${chalk.yellow("[TestLib]")} ${chalk.red("Error:")} TestContext TestMessage TestSolution \nReceived value: ${chalk.gray('{\n  "key": "value"\n}')}`,
     );
 
     consoleSpy.mockRestore();
@@ -105,6 +101,9 @@ describe("logSimple", () => {
   });
 });
 
+/**
+ * Log compact error
+ */
 describe("logCompact", () => {
   it("should log compact error with all properties", () => {
     const consoleSpy = vi.spyOn(console, "log").mockImplementation(() => {
@@ -116,13 +115,14 @@ describe("logCompact", () => {
       file: "testFile.ts",
       message: "TestMessage",
       method: "GET",
+      severity: "high",
       type: "error",
     } as HandlerErrorProperties;
 
     logCompact(data);
 
     expect(consoleSpy).toHaveBeenCalledWith(
-      `${chalk.gray(timestamp.toISOString())} ${chalk.yellow("[GET]")} ${chalk.red("Error:")} ${chalk.green("404")} ${chalk.gray("testFile.ts")} TestMessage`,
+      `${chalk.gray(timestamp.toISOString())} ${chalk.yellow("[GET]")} ${chalk.red("HIGH Error:")} ${chalk.green("404")} ${chalk.gray("testFile.ts")} TestMessage`,
     );
 
     consoleSpy.mockRestore();
@@ -155,19 +155,23 @@ describe("logCompact", () => {
       ...mockErrorData,
       message: "TestMessage",
       method: "POST",
+      severity: "high",
       type: "error",
     } as HandlerErrorProperties;
 
     logCompact(data);
 
     expect(consoleSpy).toHaveBeenCalledWith(
-      `${chalk.gray(timestamp.toISOString())} ${chalk.yellow("[POST]")} ${chalk.red("Error:")} TestMessage`,
+      `${chalk.gray(timestamp.toISOString())} ${chalk.yellow("[POST]")} ${chalk.red("HIGH Error:")} TestMessage`,
     );
 
     consoleSpy.mockRestore();
   });
 });
 
+/**
+ * Log detailed error
+ */
 describe("logDetail", () => {
   it("should log detailed error with all properties", () => {
     const consoleSpy = vi.spyOn(console, "log").mockImplementation(() => {
@@ -194,7 +198,9 @@ describe("logDetail", () => {
     );
     expect(consoleSpy).toHaveBeenCalledWith(chalk.bold(`\nDescription:`));
     expect(consoleSpy).toHaveBeenCalledWith(chalk.gray(`TestContext TestMessage`));
-    expect(consoleSpy).toHaveBeenCalledWith(chalk.gray(JSON.stringify(data.values)));
+    expect(consoleSpy).toHaveBeenCalledWith(
+      `Received value: ${chalk.gray('{\n  "key": "value"\n}')}`,
+    );
     expect(consoleSpy).toHaveBeenCalledWith(chalk.bold(`\nSolution:`));
     expect(consoleSpy).toHaveBeenCalledWith(chalk.gray(data.solution));
     expect(consoleSpy).toHaveBeenCalledWith(chalk.bold(`\nExample:`));
@@ -220,17 +226,10 @@ describe("logDetail", () => {
     logDetail(data);
 
     expect(consoleSpy).toHaveBeenCalledWith(
-      `${data.name} [${chalk.red("Error:")}] : ${chalk.bold("TestMessage")}`,
+      `${data.name} [${chalk.red("Error")}] ${chalk.bold("TestMessage")}`,
     );
     expect(consoleSpy).toHaveBeenCalledWith(chalk.bold(`\nDescription:`));
     expect(consoleSpy).toHaveBeenCalledWith(chalk.gray(` TestMessage`));
-    expect(consoleSpy).toHaveBeenCalledWith(chalk.gray(JSON.stringify(data.values)));
-    expect(consoleSpy).toHaveBeenCalledWith(chalk.bold(`\nSolution:`));
-    expect(consoleSpy).toHaveBeenCalledWith(chalk.gray(data.solution));
-    expect(consoleSpy).toHaveBeenCalledWith(chalk.bold(`\nExample:`));
-    expect(consoleSpy).toHaveBeenCalledWith(chalk.gray(data.example));
-    expect(consoleSpy).toHaveBeenCalledWith(chalk.bold(`\nStack Trace:`));
-    expect(consoleSpy).toHaveBeenCalledWith("");
 
     consoleSpy.mockRestore();
   });
@@ -250,17 +249,10 @@ describe("logDetail", () => {
     logDetail(data);
 
     expect(consoleSpy).toHaveBeenCalledWith(
-      `${data.name} [${chalk.red("Error:")}] ${chalk.green("POST")}: ${chalk.bold("TestMessage")}`,
+      `${data.name} [${chalk.red("Error")}] ${chalk.green("POST")}: ${chalk.bold("TestMessage")}`,
     );
     expect(consoleSpy).toHaveBeenCalledWith(chalk.bold(`\nDescription:`));
     expect(consoleSpy).toHaveBeenCalledWith(chalk.gray(`TestContext TestMessage`));
-    expect(consoleSpy).toHaveBeenCalledWith(chalk.gray(JSON.stringify(data.values)));
-    expect(consoleSpy).toHaveBeenCalledWith(chalk.bold(`\nSolution:`));
-    expect(consoleSpy).toHaveBeenCalledWith(chalk.gray(data.solution));
-    expect(consoleSpy).toHaveBeenCalledWith(chalk.bold(`\nExample:`));
-    expect(consoleSpy).toHaveBeenCalledWith(chalk.gray(data.example));
-    expect(consoleSpy).toHaveBeenCalledWith(chalk.bold(`\nStack Trace:`));
-    expect(consoleSpy).toHaveBeenCalledWith("");
 
     consoleSpy.mockRestore();
   });
